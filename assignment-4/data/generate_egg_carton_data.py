f83b69dc613be8a2f145243dbc8ad5f2bb0fea2a
Per Magnus Veierland <pveierland@gmail.com>
2015-10-19 23:55:14 +0200

    A4: Final delivery

--- assignment-4/data/generate_egg_carton_data.py
+++ assignment-4/data/generate_egg_carton_data.py
@@ -9,13 +9,12 @@ import numpy
 from vi.app.egg_carton import render_output, Problem
 from vi.search import simulated_annealing
 
-scenarios = [ [ 5, 5, 2 ], [ 6, 6, 2 ], [ 8, 8, 1 ], [ 10, 10, 3 ] ]
+scenarios = [ [ 5, 5, 2 ] , [ 6, 6, 2 ], [ 8, 8, 1 ], [ 10, 10, 3 ] ]
 
 start_temperatures       = list(numpy.linspace(0.1, 5, 20))
-delta_temperature_ratios = list(numpy.linspace(10, 1000, 20))
+delta_temperature_ratios = list(numpy.linspace(10, 10000, 20))
 
-max_epochs = 100000
-repeats    = 20
+repeats = 20
 
 results = numpy.zeros(
     (len(scenarios), len(start_temperatures), len(delta_temperature_ratios)))
@@ -25,17 +24,31 @@ for s, scenario in enumerate(scenarios):
 
     for i, start_temperature in enumerate(start_temperatures):
         for j, delta_temperature_ratio in enumerate(delta_temperature_ratios):
+            print('{0} {1} {2}'.format(s, i, j))
+
             delta_temperature = start_temperature / delta_temperature_ratio
 
             values = []
 
             for _ in range(repeats):
                 solution, epochs = simulated_annealing(
-                    problem, start_temperature, delta_temperature, max_epochs)
+                    problem, start_temperature, delta_temperature)
 
                 values.append(solution.sum())
 
             results[s, i, j] = numpy.mean(values)
+
+for s, scenario in enumerate(scenarios):
+    with open('egg-carton-params-scenario-{0}.txt'.format(s + 1), 'w') as output_file:
+        for i, start_temperature in enumerate(start_temperatures):
+            for j, delta_temperature_ratio in enumerate(delta_temperature_ratios):
+                print('{0:f} {1:f} {2:f}'.format(
+                    start_temperature,
+                    delta_temperature_ratio,
+                    results[s, i, j]),
+                    file=output_file)
+
+            print('', file=output_file)
 
 best_values = [None] * len(scenarios)
 best_iterations = 10000
@@ -50,14 +63,15 @@ for s, scenario in enumerate(scenarios):
     delta_temperature       = start_temperature / delta_temperature_ratio
 
     best_solution = (0,)
-    histogram     = collections.Counter()
+    num_optimal   = 0
 
     for _ in range(best_iterations):
         solution, epochs = simulated_annealing(
-            problem, start_temperature, delta_temperature, max_epochs)
+            problem, start_temperature, delta_temperature)
         solution_value = solution.sum()
 
-        histogram[solution_value] += 1
+        if problem.is_terminal(solution):
+            num_optimal += 1
 
         if solution_value > best_solution[0]:
             best_solution = (solution_value, solution)
@@ -65,9 +79,9 @@ for s, scenario in enumerate(scenarios):
     best_values[s] = (start_temperature,
                       delta_temperature,
                       best_solution[0],
-                      histogram[best_solution[0]],
+                      num_optimal,
                       best_iterations,
-                      histogram[best_solution[0]] / best_iterations)
+                      num_optimal / best_iterations)
 
     if best_solution[0] > 0:
         render_output('egg-carton-best-scenario-{0}.pdf'.format(s + 1),
@@ -79,12 +93,3 @@ with open('egg-carton-best-values.txt', 'w') as output_file:
             s + 1, *best_values[s]),
             file=output_file)
 
-for s, scenario in enumerate(scenarios):
-    with open('egg-carton-params-scenario-{0}.txt'.format(s + 1), 'w') as output_file:
-        for i, start_temperature in enumerate(start_temperatures):
-            for j, delta_temperature_ratio in enumerate(delta_temperature_ratios):
-                print('{0:f} {1} {2}'.format(
-                    start_temperature, delta_temperature_ratio, results[s, i, j]),
-                    file=output_file)
-
-            print('', file=output_file)
